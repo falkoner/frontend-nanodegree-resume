baea48bac179281d8cba68b3775f1d59c46d9ab3
Alex Filatau <filatau@gmail.com>
2015-01-03 14:10:02 -0800

    End of lesson 1 state: added data objects

---
+++ js/resumeBuilder.js
@@ -0,0 +1,84 @@
+var bio = {
+  "name"          : "Alex Fry",
+  "role"          : "Web Developer",
+  "contacts"   : {
+    "mobile"  : "650-555-5555",
+    "email"   : "afry@gmail.com",
+    "github"  : "afry",
+    "twitter" : "@afry",
+    "location": "San Jose"
+  },
+  "welcomeMessage": "Blah blah and drink!",
+  "skills"        : ["test", "code", "read", "drink liquids"],
+  "bioPic"    : "images/fry.jpg"
+};
+
+var education = {
+  "schools": [
+    {
+      "name": "Nova Scotia State University",
+      "location": "Eureca, CA",
+      "degree": "Masters",
+      "majors": ["CS"],
+      "dates": 2013,
+      "url": "http://novascotia.edu"
+    }
+  ],
+  "onlineCourses": [
+    {
+      "title": "JavaScript Syntax",
+      "school": "Udacity",
+      "dates": 2014,
+      "url": "http://www.udacity.com/course/ud804"
+    }
+  ]
+};
+
+var work = {
+  "jobs": [
+    {
+      "employer": "Sam's Long Hands Inc.",
+      "title": "Front End Developer",
+      "location": "San Jose, CA",
+      "dates": 2014,
+      "description": "n/a"
+    },
+    {
+      "employer": "Berry Cafe",
+      "title": "Barista",
+      "location": "Seattle, WA",
+      "dates": 2013,
+      "description": "n/a"
+    }
+  ]
+};
+
+var projects = [
+  {
+    "title": "Lorem Impsum",
+    "dates": 2014,
+    "description": "Ara burra ho",
+    "images": ["images/197x148.gif"]
+  }
+];
+
+// var formattedName = HTMLheaderName.replace("%data%", bio.name);
+// var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+
+// var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+// var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+// var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+// var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+// var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+
+// var formattedWelcomeMsg = HTMLWelcomeMsg.replace("%data%", bio.welcomeMessage);
+// var formattedBioPic = HTMLbioPic.replace("%data%", bio.pictureUrl);
+
+// $("#header").prepend(formattedRole);
+// $("#header").prepend(formattedName);
+
+// $("#topContacts").append(formattedLocation);
+// $("#topContacts").append(formattedGithub);
+// $("#topContacts").append(formattedTwitter);
+// $("#topContacts").append(formattedEmail);
+// $("#topContacts").append(formattedMobile);
